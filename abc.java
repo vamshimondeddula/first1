ec24ecf02143d7850eb00e99f8d6ba36c158c593
vamshimondeddula <166008803+vamshimondeddula@users.noreply.github.com>
2025-04-09 22:48:29 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -2,20 +2,14 @@ import java.util.Scanner;
 
 public class abc{
     public static void main(String[] args) {
-        // Create Scanner object to read input
+        
         Scanner scanner = new Scanner(System.in);
 
         // Print Hello World
         System.out.println("Hello, World!");
 
-        // Ask for user's name
-        System.out.print("What's your name? ");
-        String name = scanner.nextLine();
+    
+      
 
-        // Greet the user
-        System.out.println("Nice to meet you, " + name + "!");
-
-        // Close the scanner
-        scanner.close();
     }
 }
