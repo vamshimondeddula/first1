ca27dce7852f088006a5c1661e19dc239d01c8e2
vamshimondeddula <166008803+vamshimondeddula@users.noreply.github.com>
2025-04-09 22:48:43 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class abc{
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in);
-
         // Print Hello World
         System.out.println("Hello, World!");
 
